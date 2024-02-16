ff8c00cb7965ddbdac5dbdbdc88d334a7e265f68
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-02-16 12:17:53 +0100

    Updated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,96 @@
 import AgeForm from "./components/AgeForm"
 import AgeResult from "./components/AgeResult"
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+
+  const [day, setDay] = useState<number>(0)
+  const [month, setMonth] = useState<number>(0)
+  const [year, setYear] = useState<number>(0)
+
+  const handleDayChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const dayValue : number = Number(e.target.value)
+      setDay(dayValue)
+      console.log("my day",day)
+  }    
+
+  const handleMonthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const monthValue : number = Number(e.target.value)
+      setMonth(monthValue)
+  }
+
+  const handleYearChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const yearValue : number = Number(e.target.value)
+      setYear(yearValue)
+  }
+
+  useEffect(() => {
+      console.log("my day",day)
+  }, [day, month, year])
+
+  function getInputDateValues()
+  {
+      console.log("SENT IN DAY", day)
+      console.log("button clicked")
+      const inputDayValue : number = day
+      const inputMonthValue : number = month
+      const inputYearValue : number = year
+
+      return { day: inputDayValue, month: inputMonthValue, year: inputYearValue }
+      
+  }
+
+  function getCurrentDate(){
+      const date = new Date()
+
+      const currentDay : number = date.getDate()
+      const currentMonth : number = date.getMonth() +1
+      const currentYear : number = date.getFullYear()
+
+      return { day: currentDay, month: currentMonth, year: currentYear }
+
+  }
+
+  function calculateValues(inputDate: {day: number, month: number , year: number}, currentDate: {day: number, month: number, year: number}){
+
+      let ageInYears : number = currentDate.year - inputDate.year
+
+      if (currentDate.month < inputDate.month){
+          ageInYears --
+
+      }else if (currentDate.month >= inputDate.month && currentDate.day < inputDate.day){
+          ageInYears --
+
+      }else if (currentDate.month >= inputDate.month){
+          ageInYears
+      }    
+      
+      let ageInMonths : number = ageInYears * 12
+      let ageInDays : number =  ageInYears * 365
+
+
+      return {ageInYears, ageInMonths, ageInDays}
+  }
+
+  calculateValues(getInputDateValues(), getCurrentDate())
+
+  console.log(calculateValues(getInputDateValues(), getCurrentDate()))
+
+
+
   return (
     <main className="container">
-    <AgeForm/>
-    <AgeResult/>
+    <AgeForm
+    day={day}
+    month={month}
+    year={year}
+    handleDayChange={handleDayChange}
+    handleMonthChange={handleMonthChange}
+    handleYearChange={handleYearChange}
+    />
+    <AgeResult
+    calculateValues={() => calculateValues(getInputDateValues(), getCurrentDate())}
+    />
     </main>
   )
 }
--- src/components/AgeResult.tsx
+++ src/components/AgeResult.tsx
@@ -1,21 +1,33 @@
-const AgeResult = () => {
+type AgeResultProps = {
+    calculateValues: () => {
+      ageInYears: number,
+      ageInMonths: number,
+      ageInDays: number,
+    }
+  }
   
-    return (
-    <section className="age-result-container">
-        <article className="text-container">
-            <h1 className="age-number">--</h1>
-            <h1 className="age-date">years</h1>
-        </article>
-        <article className="text-container">
-            <h1 className="age-number">--</h1>
-            <h1 className="age-date">months</h1>
-        </article>
-        <article className="text-container">
-            <h1 className="age-number">--</h1>
-            <h1 className="age-date">days</h1>
-        </article>
-    </section>
-  )
-}
+  
+
+    const AgeResult: React.FC<AgeResultProps> = (props) => {
+        const { ageInYears, ageInMonths, ageInDays } = props.calculateValues();
+      
+        return (
+          <section className="age-result-container">
+            <article className="text-container">
+              <h1 className="age-number">{ageInYears}</h1>
+              <h1 className="age-date">years</h1>
+            </article>
+            <article className="text-container">
+              <h1 className="age-number">{ageInMonths}</h1>
+              <h1 className="age-date">months</h1>
+            </article>
+            <article className="text-container">
+              <h1 className="age-number">{ageInDays}</h1>
+              <h1 className="age-date">days</h1>
+            </article>
+          </section>
+        );
+      }
+      
 
 export default AgeResult
