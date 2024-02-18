99c28fccddd0376fb88345fb70ba3f49229c3e7d
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-02-18 16:34:39 +0100

    Revert "Updated code"
    This reverts commit ff8c00cb7965ddbdac5dbdbdc88d334a7e265f68.

--- src/components/AgeForm.tsx
+++ src/components/AgeForm.tsx
@@ -1,15 +1,80 @@
-type AgeFormProps = {
-    day: number,
-    month: number,
-    year: number,
-    handleDayChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    handleMonthChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    handleYearChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    calculateValues: () => {ageInYears: number, ageInMonths: number, ageInDays: number}
-  }
+import React, { useState, useEffect } from 'react';
 
-const AgeForm : React.FC<AgeFormProps> = (props) => {
+const AgeForm = () => {
     
+    const [day, setDay] = useState<number>(0)
+    const [month, setMonth] = useState<number>(0)
+    const [year, setYear] = useState<number>(0)
+
+    const handleDayChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const dayValue : number = Number(e.target.value)
+        setDay(dayValue)
+        console.log("my day",day)
+    }    
+
+    const handleMonthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const monthValue : number = Number(e.target.value)
+        setMonth(monthValue)
+    }
+
+    const handleYearChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const yearValue : number = Number(e.target.value)
+        setYear(yearValue)
+    }
+
+    useEffect(() => {
+        console.log("my day",day)
+    }, [day, month, year])
+
+    function getInputDateValues()
+    {
+        console.log("SENT IN DAY", day)
+        console.log("button clicked")
+        const inputDayValue : number = day
+        const inputMonthValue : number = month
+        const inputYearValue : number = year
+
+        return { day: inputDayValue, month: inputMonthValue, year: inputYearValue }
+        
+    }
+
+    function getCurrentDate(){
+        const date = new Date()
+
+        const currentDay : number = date.getDate()
+        const currentMonth : number = date.getMonth() +1
+        const currentYear : number = date.getFullYear()
+
+        return { day: currentDay, month: currentMonth, year: currentYear }
+
+    }
+
+    function calculateValues(inputDate: {day: number, month: number , year: number}, currentDate: {day: number, month: number, year: number}){
+
+        let ageInYears : number = currentDate.year - inputDate.year
+
+        if (currentDate.month < inputDate.month){
+            ageInYears --
+
+        }else if (currentDate.month >= inputDate.month && currentDate.day < inputDate.day){
+            ageInYears --
+
+        }else if (currentDate.month >= inputDate.month){
+            ageInYears
+        }    
+        
+        let ageInMonths : number = ageInYears * 12
+        let ageInDays : number =  ageInYears * 365
+
+
+        return {ageInYears, ageInMonths, ageInDays}
+    }
+
+    calculateValues(getInputDateValues(), getCurrentDate())
+
+    console.log(calculateValues(getInputDateValues(), getCurrentDate()))
+
+
   return (
 
     <form>
@@ -20,8 +85,8 @@ const AgeForm : React.FC<AgeFormProps> = (props) => {
         className="input"
         type="number"
         name="day"
-        value={props.day}
-        onChange={props.handleDayChange}
+        value={day}
+        onChange={handleDayChange}
         min="1"
         max="31"
         placeholder="DD"
@@ -37,8 +102,8 @@ const AgeForm : React.FC<AgeFormProps> = (props) => {
         className="input"
         type="number"
         name="month"
-        value={props.month}
-        onChange={props.handleMonthChange}
+        value={month}
+        onChange={handleMonthChange}
         min="1"
         max="12"
         placeholder="MM"
@@ -54,8 +119,8 @@ const AgeForm : React.FC<AgeFormProps> = (props) => {
         className="input"
         type="number"
         name="year"
-        value={props.year}
-        onChange={props.handleYearChange}
+        value={year}
+        onChange={handleYearChange}
         min="1900"
         max="2024"
         placeholder="YYYY"
--- src/components/AgeResult.tsx
+++ src/components/AgeResult.tsx
@@ -1,33 +1,21 @@
-interface AgeResultProps {
-  myProp: {
-    ageInYears: number;
-    ageInMonths: number;
-    ageInDays: number;
-  }
-  isVisible: boolean;
-}
+const AgeResult = () => {
   
-const AgeResult: React.FC<AgeResultProps> = ({ myProp, isVisible }) => {
-  if (!isVisible) {
-    return null;
-  }
-        return (
-          <section className="age-result-container">
-            <article className="text-container">
-              <h1 className="age-number">--{myProp.ageInYears}</h1>
-              <h1 className="age-date">years</h1>
-            </article>
-            <article className="text-container">
-              <h1 className="age-number">--{myProp.ageInMonths}</h1>
-              <h1 className="age-date">months</h1>
-            </article>
-            <article className="text-container">
-              <h1 className="age-number">--{myProp.ageInDays}</h1>
-              <h1 className="age-date">days</h1>
-            </article>
-          </section>
-        );
-      }
-      
+    return (
+    <section className="age-result-container">
+        <article className="text-container">
+            <h1 className="age-number">--</h1>
+            <h1 className="age-date">years</h1>
+        </article>
+        <article className="text-container">
+            <h1 className="age-number">--</h1>
+            <h1 className="age-date">months</h1>
+        </article>
+        <article className="text-container">
+            <h1 className="age-number">--</h1>
+            <h1 className="age-date">days</h1>
+        </article>
+    </section>
+  )
+}
 
 export default AgeResult
