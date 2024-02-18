c6cd7f101ab92ace538c0401151dec3463bc4022
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-02-18 16:35:52 +0100

    Updated code

--- src/components/AgeForm.tsx
+++ src/components/AgeForm.tsx
@@ -1,80 +1,15 @@
-import React, { useState, useEffect } from 'react';
+type AgeFormProps = {
+    day: number,
+    month: number,
+    year: number,
+    handleDayChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    handleMonthChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    handleYearChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    calculateValues: () => {ageInYears: number, ageInMonths: number, ageInDays: number}
+  }
 
-const AgeForm = () => {
+const AgeForm : React.FC<AgeFormProps> = (props) => {
     
-    const [day, setDay] = useState<number>(0)
-    const [month, setMonth] = useState<number>(0)
-    const [year, setYear] = useState<number>(0)
-
-    const handleDayChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const dayValue : number = Number(e.target.value)
-        setDay(dayValue)
-        console.log("my day",day)
-    }    
-
-    const handleMonthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const monthValue : number = Number(e.target.value)
-        setMonth(monthValue)
-    }
-
-    const handleYearChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const yearValue : number = Number(e.target.value)
-        setYear(yearValue)
-    }
-
-    useEffect(() => {
-        console.log("my day",day)
-    }, [day, month, year])
-
-    function getInputDateValues()
-    {
-        console.log("SENT IN DAY", day)
-        console.log("button clicked")
-        const inputDayValue : number = day
-        const inputMonthValue : number = month
-        const inputYearValue : number = year
-
-        return { day: inputDayValue, month: inputMonthValue, year: inputYearValue }
-        
-    }
-
-    function getCurrentDate(){
-        const date = new Date()
-
-        const currentDay : number = date.getDate()
-        const currentMonth : number = date.getMonth() +1
-        const currentYear : number = date.getFullYear()
-
-        return { day: currentDay, month: currentMonth, year: currentYear }
-
-    }
-
-    function calculateValues(inputDate: {day: number, month: number , year: number}, currentDate: {day: number, month: number, year: number}){
-
-        let ageInYears : number = currentDate.year - inputDate.year
-
-        if (currentDate.month < inputDate.month){
-            ageInYears --
-
-        }else if (currentDate.month >= inputDate.month && currentDate.day < inputDate.day){
-            ageInYears --
-
-        }else if (currentDate.month >= inputDate.month){
-            ageInYears
-        }    
-        
-        let ageInMonths : number = ageInYears * 12
-        let ageInDays : number =  ageInYears * 365
-
-
-        return {ageInYears, ageInMonths, ageInDays}
-    }
-
-    calculateValues(getInputDateValues(), getCurrentDate())
-
-    console.log(calculateValues(getInputDateValues(), getCurrentDate()))
-
-
   return (
 
     <form>
@@ -85,8 +20,8 @@ const AgeForm = () => {
         className="input"
         type="number"
         name="day"
-        value={day}
-        onChange={handleDayChange}
+        value={props.day}
+        onChange={props.handleDayChange}
         min="1"
         max="31"
         placeholder="DD"
@@ -102,8 +37,8 @@ const AgeForm = () => {
         className="input"
         type="number"
         name="month"
-        value={month}
-        onChange={handleMonthChange}
+        value={props.month}
+        onChange={props.handleMonthChange}
         min="1"
         max="12"
         placeholder="MM"
@@ -119,8 +54,8 @@ const AgeForm = () => {
         className="input"
         type="number"
         name="year"
-        value={year}
-        onChange={handleYearChange}
+        value={props.year}
+        onChange={props.handleYearChange}
         min="1900"
         max="2024"
         placeholder="YYYY"
